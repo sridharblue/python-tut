c844588ed9cc93243e83c3eafc32833fd03614d9
sridharblue <sridhark@turiyatree.com>
2020-11-20 13:47:57 +0530

    updated

--- exceptionHandling.py
+++ exceptionHandling.py
@@ -38,20 +38,51 @@ finally:
     print("entered into finally block") ##finally will always execute
 print("this program ended")
 
+
+#prob 1
+try:
+    for i in ['a','b','c']:
+        print(i**2)
+except TypeError:
+    print('Invalid operation')
+
+#prob2
+x = 5
+y = 0
+try:
+    z = x/y
+except ZeroDivisionError:
+    print('Invalid Operation')
+finally:
+    print('All Done')
+
+#prob3
+def ask():
+    while True:
+        try:
+            num = int(input('Input an integer: '))
+        except:
+            print('An error occurred! Please try again!')
+            continue
+        else:
+            print(f'Thank you, your number squared is: {num**2}')
+            break
+
+
 """ Raising Exception
     Creating our own exceptions
 """
 
 def enterage(age):
     if age<0:
-        raise ValueError("only positive numbers")
+        raise ValueError()
     if age % 2 == 0:
         print("even number")
     else:
         print("odd number")
 
 try:
-    enterage(-5)
+    enterage(-2)
 except ValueError:
     print(("only positive numbers are allowed"))
 except:
